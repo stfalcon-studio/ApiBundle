0efacc4430fd8e8632dae4904c0fd6234449b5dd
Artem Henvald <artem.genvald@stfalcon.com>
2021-11-24 15:32:33 +0200

    Add aggregate listener

---
+++ EventListener/ORM/Aggregate/AggregatePartListener.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * This file is part of the StfalconApiBundle.
+ *
+ * (c) Stfalcon LLC <stfalcon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace StfalconStudio\ApiBundle\EventListener\ORM\Aggregate;
+
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Fresh\DateTime\DateTimeHelper;
+use StfalconStudio\ApiBundle\Model\Aggregate\AggregatePartInterface;
+use StfalconStudio\ApiBundle\Model\Aggregate\AggregateRootInterface;
+
+/**
+ * AggregatePartListener.
+ */
+final class AggregatePartListener
+{
+    /** @var array<AggregateRootInterface> */
+    private array $aggregateRoots = [];
+    private DateTimeHelper $dateTimeHelper;
+
+    /**
+     * @param DateTimeHelper $dateTimeHelper
+     */
+    public function __construct(DateTimeHelper $dateTimeHelper)
+    {
+        $this->dateTimeHelper = $dateTimeHelper;
+    }
+
+    /**
+     * @param OnFlushEventArgs $eventArgs
+     */
+    public function onFlush(OnFlushEventArgs $eventArgs): void
+    {
+        $em = $eventArgs->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        foreach ($uow->getScheduledEntityUpdates() as $entity) {
+            $this->processEntity($entity);
+        }
+        foreach ($uow->getScheduledEntityInsertions() as $entity) {
+            $this->processEntity($entity);
+        }
+        foreach ($uow->getScheduledEntityDeletions() as $entity) {
+            $this->processEntity($entity);
+        }
+
+        foreach ($this->aggregateRoots as $aggregateRoot) {
+            $aggregateRoot->setUpdatedAt($this->dateTimeHelper->getCurrentDatetime());
+            $uow->recomputeSingleEntityChangeSet($em->getClassMetadata(\get_class($aggregateRoot)), $aggregateRoot);
+        }
+
+        $this->aggregateRoots = [];
+    }
+
+    /**
+     * @param object $entity
+     */
+    private function processEntity(object $entity): void
+    {
+        if ($entity instanceof AggregatePartInterface) {
+            $aggregateRoot = $entity->getAggregateRoot();
+
+            if (!\in_array($aggregateRoot, $this->aggregateRoots, true)) {
+                $this->aggregateRoots[$aggregateRoot->getId()] = $aggregateRoot;
+                $this->processEntity($aggregateRoot); // Bubble aggregate root to the top root
+            }
+        }
+    }
+}
---
+++ Tests/EventListener/ORM/Aggregate/AggregatePartListenerTest.php
@@ -0,0 +1,129 @@
+<?php
+/*
+ * This file is part of the StfalconApiBundle.
+ *
+ * (c) Stfalcon LLC <stfalcon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace StfalconStudio\ApiBundle\Tests\EventListener\ORM\Aggregate;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\UnitOfWork;
+use Fresh\DateTime\DateTimeHelper;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use StfalconStudio\ApiBundle\EventListener\ORM\Aggregate\AggregatePartListener;
+use StfalconStudio\ApiBundle\Model\Aggregate\AggregatePartInterface;
+use StfalconStudio\ApiBundle\Model\Aggregate\AggregateRootInterface;
+
+final class AggregatePartListenerTest extends TestCase
+{
+    /** @var DateTimeHelper|MockObject */
+    private $dateTimeHelper;
+
+    private AggregatePartListener $listener;
+
+    protected function setUp(): void
+    {
+        $this->dateTimeHelper = $this->createMock(DateTimeHelper::class);
+        $this->listener = new AggregatePartListener($this->dateTimeHelper);
+    }
+
+    protected function tearDown(): void
+    {
+        unset(
+            $this->listener,
+        );
+    }
+
+    public function testOnFlush(): void
+    {
+        $eventArgs = $this->createMock(OnFlushEventArgs::class);
+
+        $em = $this->createMock(EntityManager::class);
+        $eventArgs
+            ->expects(self::once())
+            ->method('getEntityManager')
+            ->willReturn($em)
+        ;
+
+        $uow = $this->createMock(UnitOfWork::class);
+        $em
+            ->expects(self::once())
+            ->method('getUnitOfWork')
+            ->willReturn($uow)
+        ;
+
+        $updatedEntity = new \stdClass();
+        $insertedEntity = $this->createMock(AggregatePartInterface::class);
+        $deletedEntity = new \stdClass();
+
+        $uow
+            ->expects(self::once())
+            ->method('getScheduledEntityUpdates')
+            ->willReturn([$updatedEntity])
+        ;
+
+        $uow
+            ->expects(self::once())
+            ->method('getScheduledEntityInsertions')
+            ->willReturn([$insertedEntity])
+        ;
+
+        $uow
+            ->expects(self::once())
+            ->method('getScheduledEntityDeletions')
+            ->willReturn([$deletedEntity])
+        ;
+
+        $insertedEntityAggregatedRoot = $this->createMock(AggregateRootInterface::class);
+        $insertedEntity
+            ->expects(self::once())
+            ->method('getAggregateRoot')
+            ->willReturn($insertedEntityAggregatedRoot)
+        ;
+
+        $insertedEntityAggregatedRootId = '123';
+        $insertedEntityAggregatedRoot
+            ->expects(self::once())
+            ->method('getId')
+            ->willReturn($insertedEntityAggregatedRootId)
+        ;
+
+        $now = new \DateTime();
+        $this->dateTimeHelper
+            ->expects(self::once())
+            ->method('getCurrentDatetime')
+            ->willReturn($now)
+        ;
+
+        $insertedEntityAggregatedRoot
+            ->expects(self::once())
+            ->method('setUpdatedAt')
+            ->with($now)
+        ;
+
+        $classMetadata = $this->createMock(ClassMetadata::class);
+        $uow
+            ->expects(self::once())
+            ->method('recomputeSingleEntityChangeSet')
+            ->with($classMetadata, $insertedEntityAggregatedRoot)
+        ;
+
+        $em
+            ->expects(self::once())
+            ->method('getClassMetadata')
+            ->with(\get_class($insertedEntityAggregatedRoot))
+            ->willReturn($classMetadata)
+        ;
+
+        $this->listener->onFlush($eventArgs);
+    }
+}
