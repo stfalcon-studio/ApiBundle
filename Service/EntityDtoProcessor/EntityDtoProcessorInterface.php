9a1b5200a2f8d37cf23e369c784ab06d9ad3b554
Artem Henvald <artem.genvald@stfalcon.com>
2023-02-07 12:38:07 +0200

    Fix namespace

--- Service/EntityDtoProcessor/EntityDtoProcessorInterface.php
+++ Service/EntityDtoProcessor/EntityDtoProcessorInterface.php
@@ -10,7 +10,7 @@
 
 declare(strict_types=1);
 
-namespace StfalconStudio\ApiBundle\Service\AttributeProcessor;
+namespace StfalconStudio\ApiBundle\Service\EntityDtoProcessor;
 
 use StfalconStudio\ApiBundle\DTO\DtoInterface;
 use StfalconStudio\ApiBundle\DTO\UpdateDtoInterface;
