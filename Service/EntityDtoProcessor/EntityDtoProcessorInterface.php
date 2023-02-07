393f5b97a7c7a15b3ed08a7530029bbada0f598f
Artem Henvald <artem.genvald@stfalcon.com>
2023-02-07 12:35:28 +0200

    Add new interfaces

---
+++ Service/EntityDtoProcessor/EntityDtoProcessorInterface.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of the StfalconApiBundle.
+ *
+ * (c) Stfalcon LLC <stfalcon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace StfalconStudio\ApiBundle\Service\AttributeProcessor;
+
+use StfalconStudio\ApiBundle\DTO\DtoInterface;
+use StfalconStudio\ApiBundle\DTO\UpdateDtoInterface;
+
+/**
+ * EntityDtoProcessorInterface.
+ */
+interface EntityDtoProcessorInterface
+{
+    public function createEntityFromDto(DtoInterface $dto): object;
+
+    public function createUpdateDtoFromEntity(object $object): UpdateDtoInterface;
+
+    public function updateEntityFromDto(object $object, UpdateDtoInterface $dto): void;
+}
