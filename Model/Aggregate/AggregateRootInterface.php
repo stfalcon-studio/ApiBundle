38af2e06d2001be59f7e2b713e55626c4b46fa40
Artem Henvald <genvaldartem@gmail.com>
2023-09-21 11:51:12 +0300

    Remove parents for AggregateRootInterface

--- Model/Aggregate/AggregateRootInterface.php
+++ Model/Aggregate/AggregateRootInterface.php
@@ -12,12 +12,9 @@ declare(strict_types=1);
 
 namespace StfalconStudio\ApiBundle\Model\Aggregate;
 
-use StfalconStudio\ApiBundle\Model\Timestampable\TimestampableInterface;
-use StfalconStudio\ApiBundle\Model\UUID\UuidInterface;
-
 /**
  * AggregateRootInterface.
  */
-interface AggregateRootInterface extends TimestampableInterface, UuidInterface
+interface AggregateRootInterface
 {
 }
